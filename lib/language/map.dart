8a52290910deb2dd0a429a919ba70be4410ba37d
Bullets-jyr <jyr@bullets.co.kr>
2023-04-20 13:59:56 +0900

    조건에 맞게 Padding 적용하기

--- lib/language/map.dart
+++ lib/language/map.dart
@@ -1,9 +1,9 @@
 void main() {
-  final number = 123;
-
-  print(number.toString().runtimeType);
-  print(number.toString().split('').runtimeType);
-  print(number.toString().split('').map((e) => '$e.jpg').toList());
+  // final number = 123;
+  //
+  // print(number.toString().runtimeType);
+  // print(number.toString().split('').runtimeType);
+  // print(number.toString().split('').map((e) => '$e.jpg').toList());
 
   final numbers = [
     123,
@@ -11,11 +11,20 @@ void main() {
     789,
   ];
 
-  print(
-    numbers.map(
-      (x) => x.toString().split('').map(
-            (y) => '$y.jpg',
-          ).toList(),
-    ).toList(),
-  );
+  // print(
+  //   numbers.map(
+  //     (x) => x.toString().split('').map(
+  //           (y) => '$y.jpg',
+  //         ).toList(),
+  //   ).toList(),
+  // );
+
+  // (MapEntry(0: 123), MapEntry(1: 456), MapEntry(2: 789))
+  print(numbers.asMap());
+  // print(numbers.asMap().entries);
+  // print(numbers.asMap().entries.runtimeType);
+  // print(numbers.asMap().entries.map((e) => print('${e.key}, ${e.value}')));
+  // print(numbers.asMap().entries.toList());
+  // print(numbers.asMap().entries.toList()[0].key);
+  // print(numbers.asMap().entries.toList()[0].value);
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -49,21 +49,28 @@ class _HomeScreenState extends State<HomeScreen> {
                     123,
                     456,
                     789,
-                  ].map(
-                    (x) => Row(
-                      children: x
-                          .toString()
-                          .split('')
-                          .map(
-                            (y) => Image.asset(
-                              'asset/img/$y.png',
-                              height: 70.0,
-                              width: 50.0,
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  ).toList(),
+                  ]
+                  .asMap()
+                  .entries
+                      .map(
+                        (x) => Padding(
+                          padding: EdgeInsets.only(bottom: x.key == 2 ? 0 : 16.0),
+                          child: Row(
+                            children: x.value
+                                .toString()
+                                .split('')
+                                .map(
+                                  (y) => Image.asset(
+                                    'asset/img/$y.png',
+                                    height: 70.0,
+                                    width: 50.0,
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                        ),
+                      )
+                      .toList(),
                 ),
                 // ),
               ),
