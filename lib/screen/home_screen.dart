24260a2987232954954e92d8e097c21163504775
Bullets-jyr <jyr@bullets.co.kr>
2023-04-20 11:00:44 +0900

    디자인 적용하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,25 +12,38 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('랜덤숫자 생성기'),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.settings,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '랜덤숫자 생성기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w700
+                    ) ,
                   ),
-                ),
-              ],
-            ),
-            Expanded(
-              // child: SizedBox(
-              //   width: double.infinity,
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.settings,
+                      color: RED_COLOR,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                // child: SizedBox(
+                //   width: double.infinity,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -39,41 +53,45 @@ class _HomeScreenState extends State<HomeScreen> {
                     Text('789'),
                   ],
                 ),
+                // ),
+              ),
+              // SizedBox: Container보다 기능이 적은 위젯
+              // SizedBox(
+              //   // 무한대
+              //   width: double.infinity,
+              //   child: ElevatedButton(
+              //     onPressed: () {},
+              //     child: Text(
+              //       '생성하기!',
+              //     ),
+              //   ),
               // ),
-            ),
-            // SizedBox: Container보다 기능이 적은 위젯
-            // SizedBox(
-            //   // 무한대
-            //   width: double.infinity,
-            //   child: ElevatedButton(
-            //     onPressed: () {},
-            //     child: Text(
-            //       '생성하기!',
-            //     ),
-            //   ),
-            // ),
-            // Row(
-            //   children: [
-            //     Expanded(
-            //       child: ElevatedButton(
-            //         onPressed: () {},
-            //         child: Text(
-            //           '생성하기!',
-            //         ),
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            Container(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  '생성하기!',
+              // Row(
+              //   children: [
+              //     Expanded(
+              //       child: ElevatedButton(
+              //         onPressed: () {},
+              //         child: Text(
+              //           '생성하기!',
+              //         ),
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: RED_COLOR,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    '생성하기!',
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
